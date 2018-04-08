89ebe16c0384e9abd26b89ea49860bf2bb086e5e
Conor <oconorr3@vt.edu>
2018-04-08 10:37:21 -0400

    Integrate Audio Analysis With Microphone Stream to main.js

--- src/main.js
+++ src/main.js
@@ -4,8 +4,38 @@
 import * as THREE from 'three';
 import {HUD} from './hud/hud-controller';
 import {Stats} from './tools/stats';
+import { CameraManager } from './visualization/camera-manager.js';
+
+import AppConfig from './config/app.config';
+import { AudioSource } from './audiostream/audio-source';
+import { AudioStream } from './audiostream/audio-stream';
+import { AudioAnalyser } from './audioanalysis/audio-analyser';
+import { AnalysedDataVisualizer } from './audioanalysis/utility/analysed-data-visualizer';
+
 import css from './main.css';
-import {CameraManager} from './visualization/camera-manager.js';
+
+///////////////////////////////////////////////
+// Audio Analysis BEGIN
+///////////////////////////////////////////////
+// Size of the fft transform performed on audio stream
+const FFT_SIZE = 512;
+
+// 1- we create the audio components required for analysis
+let audiosource = new AudioSource();
+let audiostream = new AudioStream( audiosource, FFT_SIZE, AppConfig.volume );
+let audioAnalyser = new AudioAnalyser( audiostream.getBufferSize() );
+
+let startTimer = null,
+    lastFrameTimer = null,
+    deltaTime = null;
+
+// Visual informations on the analysed data
+let visuHelper = new AnalysedDataVisualizer();
+//visuHelper.init();
+
+///////////////////////////////////////////////
+// Audio Analysis END
+///////////////////////////////////////////////
 
 var VISUALS_VISIBLE = true;
 
@@ -78,11 +108,21 @@ var windowHalfY = window.innerHeight / 2;
 var speed = 6;
 var rotationSpeed = 0.002;
 
-init();
+initAudioStream();
 animate();
 
-function init() {
+function initAudioStream() {
+  audiosource.getStreamFromMicrophone(true).then(init); //set input to be from mic by default
 
+}
+
+function init() {
+  //Set up the Audio Analysis
+  audiostream.init();
+  startTimer = new Date();
+  lastFrameTimer = startTimer;
+
+  //SetUp the Hopalong Orbit
   let sprite = new THREE.TextureLoader().load("./dist/img/galaxy.png");
 
   container = document.createElement('div');
@@ -155,7 +195,25 @@ function init() {
 
 function animate() {
   requestAnimationFrame(animate);
+  analyze();
   render();
+}
+
+function analyze() {
+  let currentTimer = new Date(),
+        deltaTime    = currentTimer - lastFrameTimer;
+
+  // we send the audio data to the analyser for analysis
+  audioAnalyser.analyse( audiostream.getAudioData(), deltaTime, currentTimer )
+  let analysedData = audioAnalyser.getAnalysedData(); // we ge the analysed data
+
+  /*console.log("Analyzed Data:\nTime Domain Data = " + analysedData.getTimedomainData());
+  console.log("\nFrequencies Data = " + analysedData.getFrequenciesData());
+  console.log("\nEnergy Data = " + analysedData.getEnergy());
+  console.log("\nEnergy Average = " + analysedData.getEnergyAverage());
+  console.log("\nMultiBand Energy = " + analysedData.getMultibandEnergy());
+  */
+  lastFrameTimer = currentTimer;
 }
 
 function render() {
