8c493a6e755755de37734562b093c3e3c1a84351
Conor <oconorr3@vt.edu>
2018-10-01 20:13:44 -0400

    Define constants for configuration in respective configuration files. Remove hardcoded configuration settings in 'configuration.js'

---
+++ src/config/orbit.config.js
@@ -0,0 +1,50 @@
+/**
+ * Global settings used by the components
+ */
+export default {
+
+  //Config for the "A" input parameter into the hopalong orbit
+  A_MIN: 2,
+
+  A_MAX: 10,
+
+  A_DEFAULT: 0.5,
+
+  A_STEP_SIZE: 0.1,
+
+  //Config for the "B" input parameter into the hopalong orbit
+  B_MIN: 0,
+
+  B_MAX: 1,
+
+  B_DEFAULT: 0.25,
+
+  B_STEP_SIZE: 0.01,
+
+  //Config for the "C" input parameter into the hopalong orbit
+  C_MIN: 5,
+
+  C_MAX: 6,
+
+  C_DEFAULT: 5.5,
+
+  C_STEP_SIZE: 0.1,
+
+  //Config for the "D" input parameter into the hopalong orbit
+  D_MIN: 0,
+
+  D_MAX: 1,
+
+  D_DEFAULT: 0.5,
+
+  D_STEP_SIZE: 0.1,
+
+  //Config for the "E" input parameter into the hopalong orbit
+  E_MIN: 0,
+
+  E_MAX: 1,
+
+  E_DEFAULT: 0.5,
+
+  E_STEP_SIZE: 0.1
+};
