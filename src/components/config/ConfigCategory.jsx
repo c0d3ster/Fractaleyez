b0d33b9a3b2477c4c2a0b86a2338054de099e023
Cody Douglass <cody.douglass@block.one>
2018-12-21 02:37:46 -0500

    individually controlled category open close functionality

--- src/components/config/ConfigCategory.jsx
+++ src/components/config/ConfigCategory.jsx
@@ -1,11 +1,13 @@
 import React from 'react'
+import classNames from 'classnames'
 
 import ConfigSlider from './ConfigSlider'
-import ConfigCheckbox from './ConfigCheckbox';
+import ConfigCheckbox from './ConfigCheckbox'
 
 export default class ConfigCategory extends React.Component {
   state = {
-    ...this.props.data
+    ...this.props.data,
+    isOpen: false
   }
 
   // this helper function allows us to re render categories only when necessary using state
@@ -19,11 +21,18 @@ export default class ConfigCategory extends React.Component {
     this.props.onChange(this.props.name, item, value)
   }
 
+  setOpen = (open) => this.setState({isOpen: open})
+
   render() {
+    const categoryContentClasses = classNames('category-content', {
+      'hide-content': !this.state.isOpen
+    })
     return(
     <div className='category-container'>
-      <h3 className='category-title'>{this.props.name} config</h3>
-      <div className='category-content'>
+      <h3 className='category-title' onClick={() => this.setOpen(!this.state.isOpen)}>
+        {this.props.name} config
+      </h3>
+      <div className={categoryContentClasses}>
         {this.mapConfigItems()}
       </div>
     </div>)
