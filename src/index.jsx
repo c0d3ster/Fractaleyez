902ea89fb590aa1daef926ab1ea5b242cb1131fb
Cody Douglass <cody.douglass@block.one>
2019-04-10 19:57:43 -0400

    Updates

--- src/index.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-
-import { ConfigProvider } from './components/config/context/ConfigProvider'
-import App from './components/App.jsx';
-
-window.onload = () => {
-  ReactDOM.render(<ConfigProvider><App/></ConfigProvider>, document.getElementById('root'));
-};
