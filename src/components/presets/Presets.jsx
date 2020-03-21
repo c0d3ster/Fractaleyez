4226618e8e14e0ce5b685bac7298d4a171713ba6
cody douglass <cody.douglass@hotmail.com>
2020-03-21 19:59:22 -0400

    add colorPortal preset

--- src/components/presets/Presets.jsx
+++ src/components/presets/Presets.jsx
@@ -15,9 +15,11 @@ class Presets extends React.Component {
           <button onClick={this.props.retrieveConfigPreset}>default</button>
           <button onClick={this.props.retrieveConfigPreset}>edgeChaseSpin</button>
           <button onClick={this.props.retrieveConfigPreset}>dispersionTunnelSpin</button>
-          <button onClick={this.props.retrieveConfigPreset}>crossheirSpin</button>            
+          <button onClick={this.props.retrieveConfigPreset}>crossheirSpin</button>  
+          <button onClick={this.props.retrieveConfigPreset}>pointerz</button>            
           <button onClick={this.props.retrieveConfigPreset}>galaxySpace</button>
           <button onClick={this.props.retrieveConfigPreset}>galaxySalad</button>
+          <button onClick={this.props.retrieveConfigPreset}>colorPortal</button>
           <button onClick={this.props.retrieveConfigPreset}>ohSprite</button>
           <button onClick={this.props.retrieveConfigPreset}>eyeChase</button>
           <button onClick={this.props.retrieveConfigPreset}>sideSwirl</button> 
