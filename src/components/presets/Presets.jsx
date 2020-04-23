4d83e36486cedb0717594b3fc2184bb6994e9bd5
cody douglass <cody.douglass@hotmail.com>
2020-04-23 13:57:02 -0400

    adding images and first round of presets for actual garbage

--- src/components/presets/Presets.jsx
+++ src/components/presets/Presets.jsx
@@ -21,7 +21,13 @@ class Presets extends React.Component {
           <button onClick={this.props.retrieveConfigPreset}>Eye Chase</button>
           <button onClick={this.props.retrieveConfigPreset}>Side Swirl</button> 
           <button onClick={this.props.retrieveConfigPreset}>Circles</button>
-          <button onClick={this.props.retrieveConfigPreset}>Weed</button>              
+          <button onClick={this.props.retrieveConfigPreset}>Weed</button>  
+          <button onClick={this.props.retrieveConfigPreset}>Actual Garbage</button>   
+          <button onClick={this.props.retrieveConfigPreset}>Actual Garbage Salad</button>   
+          <button onClick={this.props.retrieveConfigPreset}>Actual Garbage Spin</button>   
+          <button onClick={this.props.retrieveConfigPreset}>Hyper Actual Garbage</button>               
+          <button onClick={this.props.retrieveConfigPreset}>Garbage Monsters</button>
+          <button onClick={this.props.retrieveConfigPreset}>Garbage Man</button>                              
           <button onClick={this.props.retrieveConfigPreset}>Bassyndicate</button>
         </Col>
       </Row>
